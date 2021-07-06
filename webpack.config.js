903d127d9d2830623d74140fb6322fa1769c5e2c
Voloshin_Sergey <4voloshin@gmail.com>
2021-07-07 01:32:51 +0300

    [EPMRD-4]: finished configuration webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,40 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`);
+
+const optimization = () => {
+  const configObj = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (isProd) {
+    configObj.minimizer = [new OptimizeCssAssetsWebpackPlugin(), new TerserWebpackPlugin()];
+  }
+
+  return configObj;
+};
 
 module.exports = {
-  entry: path.join(__dirname, './src/index.js'),
+  context: path.resolve(__dirname, 'src'),
   mode: 'development',
+  entry: './index.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: `${filename('js')}`,
   },
   module: {
     rules: [
+      { test: /\.html$/, loader: 'html-loader' },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -25,26 +48,53 @@ module.exports = {
       },
       {
         test: /\.(?:ico|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: `${filename('[ext]')}`,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+            },
+          },
+          'css-loader',
+        ],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, './dist'),
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'dist'),
     open: true,
+    compress: true,
+    hot: true,
     port: 3000,
   },
+  devtool: isProd ? false : 'source-map',
+  optimization: optimization(),
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, './src/index.html'),
+      template: path.resolve(__dirname, 'src/index.html'),
       filename: 'index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
     }),
     new CleanWebpackPlugin(),
     new ESLintPlugin(),
-    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: `${filename('css')}` }),
   ],
 };
