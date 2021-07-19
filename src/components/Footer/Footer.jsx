386968b842d57b1e190129788d823a1968fd2ca9
Voloshin_Sergey <4voloshin@gmail.com>
2021-07-19 18:41:03 +0300

    [EPMRD-5]: added Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Footer.module.scss';
+
+const Footer = () => (
+  <footer className={style.footer}>
+    <p className={style.title}>Movieroulette</p>
+  </footer>
+);
+
+export default Footer;
--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './style.scss';
 import Header from './components/Header/Header.jsx';
 import Main from './components/Main/Main.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 ReactDOM.render(
   <>
     <Header />
     <Main />
+    <Footer />
   </>,
   document.getElementById('root'),
 );
