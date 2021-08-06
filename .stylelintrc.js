8e41de539670d92789164fd717486e5f19b55536
Voloshin Sergei <4voloshin@gmail.com>
2021-08-06 13:32:56 +0300

    [EPMRD-6]: added styleguide and fixed errors

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["stylelint-config-standard",
+            "stylelint-config-rational-order",
+            ],
+  plugins: ["stylelint-order", "stylelint-scss"],
+  rules: {
+    "no-empty-source": null
+  }
+};
