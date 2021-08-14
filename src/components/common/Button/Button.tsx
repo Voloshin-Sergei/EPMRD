ae7954b2ef8a2ad36dca2ee117443989b76dcb9f
Voloshin_Sergey <4voloshin@gmail.com>
2021-08-14 12:08:35 +0300

    [EPMRD-7]: refactored Button component TS syntax

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './Button.module.scss';
+
+interface ButtonProps {
+  className: string,
+  variant: 'primary' | 'secondary',
+  text: string,
+  type: 'button' | 'submit'
+
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  className, variant = 'primary', text, type = 'button',
+}) => (
+  <button className={`${style.button} ${className} ${style[variant]}`} type={type}>
+    {text}
+  </button>
+);
