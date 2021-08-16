86ff94b439c561bbf0d296d021c5c9b1b64a3db8
Voloshin_Sergey <4voloshin@gmail.com>
2021-08-16 23:51:07 +0300

    [EPMRD-7]: refactored Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './Footer.module.scss';
 
-export const Footer: React.FunctionComponent = () => (
+export const Footer: React.FC = () => (
   <footer className={style.footer}>
     <p className={style.title}>Movieroulette</p>
   </footer>
