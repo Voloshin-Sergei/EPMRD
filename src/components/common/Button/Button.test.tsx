090433cefdf4fdf375ffaf46f33ee92d27ccbb2c
Voloshin_Sergey <4voloshin@gmail.com>
2021-09-04 10:07:34 +0300

    [EPMRD-9]: refactored test for Button component

--- src/components/common/Button/Button.test.tsx
+++ src/components/common/Button/Button.test.tsx
@@ -3,13 +3,8 @@ import { shallow } from 'enzyme';
 import { Button } from './Button';
 
 describe("Button component", () => {
-    it('should render Button component with props', () => {
-    const component = shallow(<Button className="test class" variant="secondary" children="test button" type="submit" />);
-    expect(component).toMatchSnapshot();
-  });
-
-  it('should render Button component with out props', () => {
-    const component = shallow(<Button className="test class" children="test button"/>);
+  it('should render Button component', () => {
+    const component = shallow(<Button className="test_class" >{'test button'}</Button>);
     expect(component).toMatchSnapshot();
   });
 });
