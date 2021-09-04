63caff88d363db9bf02354fe1aaa8f109463982e
Voloshin_Sergey <4voloshin@gmail.com>
2021-09-04 09:57:02 +0300

    [EPMRD-9]: refactored tests for Search component

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { render } from 'enzyme';
-import { Search }  from './Search';
+import { Search } from './Search';
+import { Button } from 'Components/common/Button';
 
-const component = render(<Search/>);
 
-describe("Search component", () => {
-    it('should render Search component', () => {
+describe('Search component', () => {
+  const component = render(<Search />);
+
+  it('should render Search component', () => {
     expect(component).toMatchSnapshot();
   });
-  it('should render 3 buttons', () => {
-    const buttons = component.find("button")
-    expect(buttons).toHaveLength(3);
+
+  it('should contain search button', () => {
+    expect(component.find('button').text().includes('search')).toBe(true);
+  });
+
+  it('should render search tag buttons on given array', () => {
+    const tagsList = ['one', 'two'];
+    const renderList = [];
+
+    tagsList
+      .map((listItem) => <Button className="tag">{listItem}</Button>)
+      .forEach((item) => renderList.push(item));
+
+    expect(renderList.length).toEqual(tagsList.length);
+
+    expect(component.find('ul').children()).toHaveLength(renderList.length);
   });
 });
