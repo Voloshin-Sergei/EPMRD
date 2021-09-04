782ecefa0a10f7085e106afc8281f618a4de6f18
Voloshin_Sergey <4voloshin@gmail.com>
2021-09-04 09:59:39 +0300

    [EPMRD-9]: refactored test for MovieCard component

--- src/components/Main/MoviesList/MovieCard/MovieCard.test.tsx
+++ src/components/Main/MoviesList/MovieCard/MovieCard.test.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { MovieCard } from './MovieCard';
 
-const props = {
+describe("MovieCard component", () => {
+  const props = {
   year: "2021",
   title: "test title",
   poster: "test cover",
   genres: ["test genre_1", "test genres_2"]
 }
 
-describe("MovieCard component", () => {
-    it('should render MovieCard component', () => {
+  it('should render MovieCard component', () => {
     const component = shallow(<MovieCard {...props}/>);
     expect(component).toMatchSnapshot();
   });
