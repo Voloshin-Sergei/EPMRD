54d478e6144ccaeb30f232e38675f06160e7e3cf
Voloshin_Sergey <4voloshin@gmail.com>
2021-09-06 22:42:28 +0300

    [EPMRD-9]: refactored Button component and test

--- src/components/common/Button/Button.test.tsx
+++ src/components/common/Button/Button.test.tsx
@@ -4,7 +4,22 @@ import { Button } from './Button';
 
 describe("Button component", () => {
   it('should render Button component', () => {
-    const component = shallow(<Button className="test_class" >{'test button'}</Button>);
+    const component = shallow(<Button>{'test button'}</Button>);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render Button component with className props', () => {
+    const component = shallow(<Button className="test_class">{'test button'}</Button>);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render Button component with variant props', () => {
+    const component = shallow(<Button variant="secondary">{'test button'}</Button>);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render Button component with type props', () => {
+    const component = shallow(<Button type="submit">{'test button'}</Button>);
     expect(component).toMatchSnapshot();
   });
 });
--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import style from './Button.module.scss';
 
 export interface ButtonProps {
-  className: string,
+  className?: string,
   variant?: 'primary' | 'secondary',
   children: React.ReactNode,
   type?: 'button' | 'submit'
 }
 
 export const Button: React.FC<ButtonProps> = ({
-  className, variant = 'primary', children, type = 'button',
+  className='', variant = 'primary', children, type = 'button',
 }) => (
   <button className={`${style.button} ${className} ${style[variant]}`} type={type}>
     {children}
