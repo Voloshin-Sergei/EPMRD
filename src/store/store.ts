c9e7eac23375d182b5afd98e8c893620ed81697a
Voloshin Sergei <4voloshin@gmail.com>
2021-12-14 12:46:48 +0300

    [EPMRD-10.1]: refactored store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,21 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, Store, Action } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import rootReducer, { RootState } from './reducers';
+import { SearchMoviesActionTypes } from './types';
 
 const persistConfig = {
   key: 'root',
   storage,
 };
 
-const persistedReducer = persistReducer<RootState>(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // eslint-disable-next-line
 export const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)));
+export const store: Store<RootState, Action<SearchMoviesActionTypes>> = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
 export const persistor = persistStore(store);
-// export type RootState = ReturnType<typeof store.getState>;
