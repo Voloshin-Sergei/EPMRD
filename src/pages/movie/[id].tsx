3f081941cc3a6df03316108540c4beda2766e6ed
Voloshin Sergei <4voloshin@gmail.com>
2022-02-22 11:26:24 +0300

    [EPMRD-13]: fixed getServerSideProps function

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { GetServerSideProps } from 'next';
+import { api } from 'API/api';
 import { MovieInfo } from 'Components/Header/MovieInfo';
 import { Movie } from 'Components/Main/MoviesList';
 import { Layout } from 'Components/layout';
 
 interface DynamicPageProps {
-  data: Movie;
+  movie: Movie;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { id } = context.params!;
-  const response = await fetch(`https://reactjs-cdp.herokuapp.com/movies/${id}`);
-  const data = await response.json();
+  const data = await api.getMovie(id as string);
+  const movie = data.data;
 
   if (!data) {
     return {
@@ -20,14 +21,14 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   }
 
   return {
-    props: { data },
+    props: { movie },
   };
 };
 
-const DynamicPage: React.FC<DynamicPageProps> = ({ data }) => {
+const DynamicPage: React.FC<DynamicPageProps> = ({ movie }) => {
   return (
     <Layout>
-      <MovieInfo movie={data} />
+      <MovieInfo movie={movie} />
     </Layout>
   );
 };
